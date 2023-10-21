673b88c91e0081fe672b54fbd546e69d584a67c2
Luis Alberto Espinal Moran <laem024097@gmail.com>
2023-10-21 18:36:03 -0400

    sustitui el contenido de index.css por normalize para eliminar formatos inecesarios y creer el primer to do item

--- src/layouts/App.jsx
+++ src/layouts/App.jsx
@@ -1,6 +1,19 @@
 import '../styles/App.css'
 
 function App() {
+  return(
+    <ToDoItem/>
+  )
+}
+
+function ToDoItem(){
+  return(
+    <li>
+      <span>v</span>
+      <p>aprender react</p>
+      <span>x</span>
+    </li>
+  )
 }
 
 export default App
